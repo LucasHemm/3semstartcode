c169d9617f83c775f4de2e809cd2c93f8d51b72f
Lucas Hemmingsen <99349701+LucasHemm@users.noreply.github.com>
2023-04-18 15:32:52 +0200

    kinda atomic commit, migrating from other project cause of issues

--- src/main/java/facades/JokeFacade.java
+++ src/main/java/facades/JokeFacade.java
@@ -52,21 +52,23 @@ public class JokeFacade {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         connection.setRequestProperty("Accept", "application/json"); // Add this line
+        StringBuilder response = new StringBuilder();
 
+        try{
         int responseCode = connection.getResponseCode();
         if (responseCode == HttpURLConnection.HTTP_OK) {
             BufferedReader in = new BufferedReader(new InputStreamReader(
                     connection.getInputStream()));
             String inputLine;
-            StringBuilder response = new StringBuilder();
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
             in.close();
-            return response.toString();
-        } else {
-            throw new IOException("Failed to fetch data from API. Response code: " + responseCode);
+        } }catch (Exception e){
+             e.printStackTrace();
         }
+        return response.toString();
+
     }
 
 
